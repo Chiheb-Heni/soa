21687c81ddbb2fb7830a499c55a8bc6aee88f036
Chiheb-Heni <98756645+Chiheb-Heni@users.noreply.github.com>
2023-10-17 19:43:39 +0100

    Add files via upload

---
+++ Tp2-SOA/src/Service/BanqueService.java
@@ -0,0 +1,15 @@
+package Service;
+
+public class BanqueService {
+public double conversion(double montant) {
+	return montant*3*3;
+}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+BanqueService b= new BanqueService();
+double montant = 0;
+System.out.println("Le montant aprés la conversion est :"+b.conversion(montant));
+	}
+
+
